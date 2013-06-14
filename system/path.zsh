6c9a712acbaefa57a6222dba9abaca5522bcf2b9
Randall A. Gordon <randall@randallagordon.com>
2013-06-13 20:20:46 -0700

    Add DoctorJS to path

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:$HOME/.rbenv/shims:/usr/local/bin:/usr/local/sbin:$HOME/.sfs:$ZSH/bin:$PATH"
 
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+
+export NODE_PATH="/usr/local/lib/jsctags:$NODE_PATH"
