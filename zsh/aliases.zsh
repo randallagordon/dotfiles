fbe2074a7e29490727c02a4c8003356eda2434a1
Randall A. Gordon <randall@randallagordon.com>
2013-06-25 13:19:37 -0700

    Add 'sagi' alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,12 @@
 alias reload!='. ~/.zshrc'
+
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias ......="cd ../../../../.."
+alias .......="cd ../../../../../.."
+alias ........="cd ../../../../../../.."
+alias .........="cd ../../../../../../../.."
+
+alias sagi="sudo apt-get install"
