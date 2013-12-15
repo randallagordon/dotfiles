027195785b28394279ef4e2c6f86b16b907cddb0
Randall A. Gordon <randall@randallagordon.com>
2013-12-14 20:56:17 -0800

    Add vim-style mode indicator

---
+++ powerline-config/localsegments.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+
+
+import os
+import subprocess
+import re
+
+
+from powerline.theme import requires_segment_info
+
+
+def get_git_status():
+    has_pending_commits = True
+    has_untracked_files = False
+    detached_head = False
+    origin_position = ""
+    current_branch = ''
+    output = subprocess.Popen(['git', 'status', '-unormal'], stdout=subprocess.PIPE).communicate()[0]
+    for line in output.split('\n'):
+        origin_status = re.findall("Your branch is (ahead|behind).*?(\d+) comm", line)
+        if len(origin_status) > 0:
+            origin_position = " %d" % int(origin_status[0][1])
+            if origin_status[0][0] == 'behind':
+                origin_position += '⇣'
+            if origin_status[0][0] == 'ahead':
+                origin_position += '⇡'
+
+        if line.find('nothing to commit') >= 0:
+            has_pending_commits = False
+        if line.find('Untracked files') >= 0:
+            has_untracked_files = True
+        if line.find('Not currently on any branch') >= 0:
+            detached_head = True
+        if line.find('On branch') >= 0:
+            current_branch = re.findall('On branch ([^ ]+)', line)[0]
+    return has_pending_commits, has_untracked_files, origin_position, detached_head, current_branch
+
+
+@requires_segment_info
+def branch(pl, segment_info):
+    #cmd = "git branch 2> /dev/null | grep -e '\\*'"
+    p1 = subprocess.Popen(['git', 'branch'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p2 = subprocess.Popen(['grep', '-e', '\\*'], stdin=p1.stdout, stdout=subprocess.PIPE)
+    output = p2.communicate()[0].strip()
+    if len(output) == 0:
+        return None
+
+    has_pending_commits, has_untracked_files, origin_position, detached_head, current_branch = get_git_status()
+
+    if len(current_branch) > 0:
+        branch = current_branch
+    elif detached_head:
+        branch = subprocess.Popen(['git', 'describe', '--all', '--contains', '--abbrev=4', 'HEAD'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        branch = '((' + branch.communicate()[0].strip() + '))'
+    else:
+        branch = 'master'
+
+    branch += origin_position
+
+    if has_untracked_files:
+        branch += ' +'
+
+    scol = ['branch']
+
+    scol.insert(0, 'branch_dirty' if has_pending_commits else 'branch_clean')
+
+    if len(current_branch) > 0:
+        return [{
+            'contents': branch,
+            'highlight_group': scol,
+        }]
+    else:
+        return None
+
+
+@requires_segment_info
+def cwd(pl, segment_info, dir_shorten_len=None, dir_limit_depth=None, use_path_separator=False):
+        try:
+                cwd = segment_info['getcwd']()
+        except OSError as e:
+                if e.errno == 2:
+                        # user most probably deleted the directory
+                        # this happens when removing files from Mercurial repos for example
+                        pl.warn('Current directory not found')
+                        cwd = "[not found]"
+                else:
+                        raise
+        home = segment_info['home']
+        if home:
+                cwd = re.sub('^' + re.escape(home), '~', cwd, 1)
+        cwd_split = cwd.split(os.sep)
+        cwd_split_len = len(cwd_split)
+        if dir_limit_depth and cwd_split_len > dir_limit_depth + 1:
+                del(cwd_split[0:-dir_limit_depth])
+                cwd_split.insert(0, '⋯')
+        cwd = [i[0:dir_shorten_len] if dir_shorten_len and i else i for i in cwd_split[:-1]] + [cwd_split[-1]]
+        ret = []
+        if not cwd[0]:
+                cwd[0] = '/'
+        draw_inner_divider = not use_path_separator
+        for part in cwd:
+                if not part:
+                        continue
+                if use_path_separator:
+                        part += os.sep
+                ret.append({
+                        'contents': part,
+                        'divider_highlight_group': 'cwd:divider',
+                        'draw_inner_divider': draw_inner_divider,
+                })
+        ret[-1]['highlight_group'] = ['cwd:current_folder', 'cwd']
+        if use_path_separator:
+                ret[-1]['contents'] = ret[-1]['contents'][:-1]
+
+        return ret
+
+
+def mode(pl):
+    ret = [{}]
+    ret[0]['contents'] = '$'
+
+    vimode = os.getenv('VIMODE', '')
+    if vimode.startswith("insert"):
+        ret[0]['highlight_group'] = 'mode_insert'
+    elif vimode.startswith("command"):
+        ret[0]['highlight_group'] = 'mode_command'
+
+    return ret
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,6 @@
+# Ensure custom segments are in the path
+export PYTHONPATH="$HOME/.dotfiles/powerline-config:$PYTHONPATH"
+
 if [ -d "$HOME/.local/bin" ]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
@@ -6,127 +9,22 @@ if [[ -r ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.z
     source ~/.local/lib/python2.7/site-packages/powerline/bindings/zsh/powerline.zsh
 fi
 
-#autoload colors && colors
-## cheers, @ehrenmurdick
-## http://github.com/ehrenmurdick/config/blob/master/zsh/prompt.zsh
+if [[ "$VIMODE_COMMAND" == "" ]]; then
+	VIMODE_COMMAND="command"
+fi
 
-#if (( $+commands[git] ))
-#then
-  #git="$commands[git]"
-#else
-  #git="/usr/bin/git"
-#fi
+if [[ "$VIMODE_INSERT" == "" ]]; then
+	VIMODE_INSERT="insert"
+fi
 
-#git_branch() {
-  #echo $($git symbolic-ref HEAD 2>/dev/null | awk -F/ {'print $NF'})
-#}
-
-#git_dirty() {
-  #st=$($git status 2>/dev/null | tail -n 1)
-  #if [[ $st == "" ]]
-  #then
-    #echo ""
-  #else
-    #if [[ "$st" =~ ^nothing ]]
-    #then
-      #echo "on %{$fg_bold[green]%}$(git_prompt_info)%{$reset_color%}"
-    #else
-      #echo "on %{$fg_bold[red]%}$(git_prompt_info)%{$reset_color%}"
-    #fi
-  #fi
-#}
-
-#git_prompt_info () {
- #ref=$($git symbolic-ref HEAD 2>/dev/null) || return
-## echo "(%{\e[0;33m%}${ref#refs/heads/}%{\e[0m%})"
- #echo "${ref#refs/heads/}"
-#}
-
-#unpushed () {
-  #$git cherry -v @{upstream} 2>/dev/null
-#}
-
-#need_push () {
-  #if [[ $(unpushed) == "" ]]
-  #then
-    #echo " "
-  #else
-    #echo " with %{$fg_bold[magenta]%}unpushed%{$reset_color%} "
-  #fi
-#}
-
-#rb_prompt(){
-  #if (( $+commands[rbenv] ))
-  #then
-		#echo "%{$fg_bold[yellow]%}$(rbenv version | awk '{print $1}')%{$reset_color%}"
-	#else
-		#echo ""
-  #fi
-#}
-
-## This keeps the number of todos always available the right hand side of my
-## command line. I filter it to only count those tagged as "+next", so it's more
-## of a motivation to clear out the list.
-#todo(){
-  #if (( $+commands[todo.sh] ))
-  #then
-    #num=$(echo $(todo.sh ls +next | wc -l))
-    #let todos=num-2
-    #if [ $todos != 0 ]
-    #then
-      #echo "$todos"
-    #else
-      #echo ""
-    #fi
-  #else
-    #echo ""
-  #fi
-#}
-
-#directory_name() {
-  #echo "%{$fg_bold[cyan]%}${PWD/#$HOME/~}%{$reset_color%}"
-#}
-
-#username() {
-  #echo "%{$fg_bold[grey]%}%n%{$reset_color%}"
-#}
-
-#host() {
-  #echo "%{$fg_bold[grey]%}@%m%{$reset_color%}"
-#}
-
-##export PROMPT=$'\n$(rb_prompt) in $(directory_name) $(git_dirty)$(need_push)\n$(vi_mode) '
-#export PROMPT=$'\n$(username)$(host):$(directory_name) $(git_dirty)$(need_push)$(vi_mode) '
-#set_prompt () {
-  #export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
-#}
-
-#precmd() {
-  #title "zsh" "%m" "%55<...<%~"
-  #set_prompt
-#}
-
-## vi mode
-#vi_mode() {
-  #echo "${VIMODE}"
-#}
-
-#if [[ "$VIMODE_COMMAND" == "" ]]; then
-  #VIMODE_COMMAND="%{$fg_bold[red]%}>>%{$reset_color%}"
-#fi
-
-#if [[ "$VIMODE_INSERT" == "" ]]; then
-  #VIMODE_INSERT="%{$fg_bold[green]%}>>%{$reset_color%}"
-#fi
-
-#function zle-line-init zle-line-finish zle-keymap-select {
-  #VIMODE="${${KEYMAP/vicmd/$VIMODE_COMMAND}/(main|viins)/$VIMODE_INSERT}"
-  #zle reset-prompt
-  #zle -R
-#}
-#zle -N zle-line-init
-#zle -N zle-line-finish
-#zle -N zle-keymap-select
+function zle-line-init zle-line-finish zle-keymap-select {
+	export VIMODE="${${KEYMAP/vicmd/$VIMODE_COMMAND}/(main|viins)/$VIMODE_INSERT}"
+	zle reset-prompt
+	zle -R
+}
+zle -N zle-line-init
+zle -N zle-line-finish
+zle -N zle-keymap-select
 
 bindkey -a u undo
 bindkey -a '^R' redo
