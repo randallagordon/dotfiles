c5e761fb3805144666ff4e008214ce25889e2df1
Randall A. Gordon <randall@randallagordon.com>
2015-01-27 14:33:21 -0800

    Comment out pbcopy/paste alises. TODO: reenable with a check for OSX

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -11,8 +11,8 @@ alias .........="cd ../../../../../../../.."
 
 alias sagi="sudo apt-get install"
 
-alias pbcopy='xclip -selection clipboard'
-alias pbpaste='xclip -selection clipboard -o'
+#alias pbcopy='xclip -selection clipboard'
+#alias pbpaste='xclip -selection clipboard -o'
 
 alias ni="node-inspector"
 alias ni8043="node-inspector --web-port=8043"
