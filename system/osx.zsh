3fd6c21b5b1c47edfb72e733319401a862d3644c
Randall A. Gordon <randall@randallagordon.com>
2015-01-27 14:31:30 -0800

    Add some OSX specific aliases

---
+++ system/osx.zsh
@@ -0,0 +1,3 @@
+alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
+alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
+alias owd='open `pwd`'
