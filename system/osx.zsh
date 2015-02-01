63ab2e14df2d9be3f7388e13680105d24b449115
Randall A. Gordon <randall@randallagordon.com>
2015-01-31 17:52:04 -0800

    Remove powerline config until I can recustomize with new format

--- system/osx.zsh
+++ system/osx.zsh
@@ -1,3 +1,7 @@
+# Tell OSX where to find Byobu settings
+: ${BYOBU_PREFIX=$(brew --prefix)}
+export BYOBU_PREFIX
+
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
 alias owd='open `pwd`'
