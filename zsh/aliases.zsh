3777ac6f0f9b3dc831d28dbd39a8c8a36751af1d
Randall A. Gordon <randall@randallagordon.com>
2015-02-08 21:09:36 -0800

    Add mouse fix for mosh

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,3 +18,6 @@ alias ni="node-inspector"
 alias ni8043="node-inspector --web-port=8043"
 
 alias ccat="pygmentize -O style=monokai -f console256 -g"
+
+# Use the mosh mouse fix
+alias mosh="moshm"
