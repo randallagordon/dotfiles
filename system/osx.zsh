e17ed8b9a53ff38e54c2f4304b05815379466a11
Randall A. Gordon <randall@randallagordon.com>
2015-07-21 16:57:46 -0700

    Set ANDROID_HOME on OSX

--- system/osx.zsh
+++ system/osx.zsh
@@ -2,6 +2,8 @@
 : ${BYOBU_PREFIX=$(brew --prefix)}
 export BYOBU_PREFIX
 
+export ANDROID_HOME='/Users/randall/Library/Android/sdk'
+
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
 alias owd='open `pwd`'
