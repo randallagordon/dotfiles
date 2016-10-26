4d254ae55d63e818a61b002280f38bbc30cb329c
Randall A. Gordon <randall@randallagordon.com>
2016-10-26 14:22:42 -0700

    NVM auto-version switching based on .nvmrc

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -2,3 +2,12 @@
 eval NVM_DIR="~/.nvm"
 export NVM_DIR
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+
+# Auto change the nvm version based on a .nvmrc file based on the current directory. See https://github.com/creationix/nvm/issues/110#issuecomment-190125863
+autoload -U add-zsh-hook
+load-nvmrc() {
+  if [[ -f .nvmrc && -r .nvmrc ]]; then
+    nvm use
+  fi
+}
+add-zsh-hook chpwd load-nvmrc
