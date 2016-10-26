5f9bed6c34128eb1bc9f4d80a31c6194bccadc43
Randall A. Gordon <randall@randallagordon.com>
2016-10-26 14:23:34 -0700

    Add OSX specific Android SDK env vars

--- system/osx.zsh
+++ system/osx.zsh
@@ -3,6 +3,9 @@
 export BYOBU_PREFIX
 
 export ANDROID_HOME='/Users/randall/Library/Android/sdk'
+export ANDROID_SDK="/Users/randall/Library/Android/sdk/tools/"
+export ANDROID_SDK_PLATFORM="/Users/randall/Library/Android/sdk/platform-tools/"
+export PATH="$PATH:$ANDROID_HOME:$ANDROID_SDK:$ANDROID_SDK_PLATFORM"
 
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
