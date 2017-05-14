409d16e89587504c90e17f1a3123253f98933309
Randall A. Gordon <randall@randallagordon.com>
2017-05-14 16:21:46 -0700

    More random crap...

--- node/aliases.zsh
+++ node/aliases.zsh
@@ -1,3 +1,4 @@
 alias npmr="npm -s run"
 alias lnm="find . -name "node_modules" -type d -prune"
 alias nnm="find . -name "node_modules" -type d -prune -exec rm -rf '{}' +"
+alias nnmi="nnm && npm i"
