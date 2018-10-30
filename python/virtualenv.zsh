c1168b0d20cc2c2bf66286461bb9c9700dd170fc
Randall A. Gordon <randall@randallagordon.com>
2018-10-29 19:38:03 -0700

    Fix for explicitly using Python 2 for Powerline

--- python/virtualenv.zsh
+++ python/virtualenv.zsh
@@ -1,4 +1,7 @@
 export WORKON_HOME=~/.envs
 
+# OSX Python fix
+export VIRTUALENVWRAPPER_PYTHON=$(brew --prefix)/opt/python2/bin/python
+
 # Pull in virtualenvwrapper
 source /usr/local/bin/virtualenvwrapper.sh
