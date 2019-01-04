d2ab0929f213e655eb1a8503f2cd1d552ec49e27
Randall A. Gordon <randall@randallagordon.com>
2019-01-04 13:14:25 -0800

    Update nvm instantiation to new version

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,7 +1,7 @@
 # Activate NVM
-eval NVM_DIR="~/.nvm"
-export NVM_DIR
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 # Auto change the nvm version based on a .nvmrc file based on the current directory. See https://github.com/creationix/nvm/issues/110#issuecomment-190125863
 autoload -U add-zsh-hook
