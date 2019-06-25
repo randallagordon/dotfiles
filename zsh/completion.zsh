5e17bb5820b3435367891c9c9f6f11fdd9bab362
Randall A. Gordon <randall@randallagordon.com>
2019-06-25 10:42:14 -0700

    Turn on menu style completions

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -4,5 +4,5 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
 
-# Add npm completion
-. <(npm completion)
+# menu-y goodness
+zstyle ':completion:*' menu yes select
