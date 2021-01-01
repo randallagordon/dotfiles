172a60abd012ad9ca46fb9ce8997d2941db7996c
Randall A. Gordon <randall@randallagordon.com>
2020-12-31 18:18:28 -0800

    Update .brewfile; OSX paths

--- system/osx.zsh
+++ system/osx.zsh
@@ -2,10 +2,11 @@
 : ${BYOBU_PREFIX=$(brew --prefix)}
 export BYOBU_PREFIX
 
+export JAVA_HOME=\$(/usr/libexec/java_home)
 export ANDROID_HOME='/Users/randall/Library/Android/sdk'
 export ANDROID_SDK="/Users/randall/Library/Android/sdk/tools/"
 export ANDROID_SDK_PLATFORM="/Users/randall/Library/Android/sdk/platform-tools/"
-export PATH="$PATH:$ANDROID_HOME:$ANDROID_SDK:$ANDROID_SDK_PLATFORM"
+export PATH="$PATH:$ANDROID_HOME:$ANDROID_SDK:$ANDROID_SDK_PLATFORM:/usr/local/opt/openjdk/bin:~/Library/Python/2.7/bin"
 
 alias showFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder'
 alias hideFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder'
